c709d9c4c62ffb83635e267e101a2624104cfb17
ervikashverma <er.vikashverma@yahoo.com>
2019-11-03 14:40:56 +0530

    first commit

---
+++ Part-2.py
@@ -0,0 +1,33 @@
+"""msg = '' 
+while msg != 'quit':     
+	msg = input("What's your message? ")     
+	print(msg) 
+
+
+x=[1,2,4,7,8,9]
+y=x.copy()
+print(y)
+x.clear()
+print(x)
+print("end")
+
+x={"1":"Vikash","2":"Verma"}
+print(x.get("1"))
+for item in x:
+	print(x.get(item))
+	
+for x in range(3,5):
+	print(x)
+	
+"""
+list=[1,2,4,7,8,9]
+for x in list:
+	print(x)
+	
+	
+	
+	
+	
+	
+	
+	
